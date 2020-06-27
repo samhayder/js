35a26853f344f69c8c396b28feecd0fb19c765df
samhayder <sams.seul@gmail.com>
2020-06-27 23:43:02 +0600

    Two fo 16/Custom Function

--- maximilianSchwarzmüller/app.js
+++ maximilianSchwarzmüller/app.js
@@ -1,4 +1,15 @@
-let currentResult = 0;
-currentResult += 100;
+const defaultResult = 0;
+let currentResult = defaultResult;
 
-outputResult(currentResult, '');
+function add(num1, num2) {
+  const result = num1 + num2;
+  alert('This is result:' + result);
+}
+
+add(4, 5);
+add(9, 6);
+currentResult = ((currentResult + 10) * 3) / 4 - 1;
+
+let calculationInfo = `(${defaultResult} +10) *3 % 4`;
+
+outputResult(currentResult, calculationInfo);
