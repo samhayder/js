50de0fb6011ecae95d37c2e8c73965c17557cf86
samhayder <sams.seul@gmail.com>
2020-06-28 22:40:10 +0600

    Two of 30/all button function

--- maximilianSchwarzmüller/app.js
+++ maximilianSchwarzmüller/app.js
@@ -1,10 +1,35 @@
 const defaultResult = 0;
 let currentResult = defaultResult;
 
-function add(num1, num2) {
+function add() {
   const calculateDescription = `${currentResult} + ${userInput.value}`;
-  currentResult = currentResult + parseFloat(userInput.value);
+  currentResult += parseFloat(userInput.value);
+  userInput.value = '';
+  outputResult(currentResult, calculateDescription);
+}
+
+function subtract() {
+  const calculateDescription = `${currentResult} - ${userInput.value}`;
+  currentResult -= parseFloat(userInput.value);
+  userInput.value = '';
+  outputResult(currentResult, calculateDescription);
+}
+
+function multiply() {
+  const calculateDescription = `${currentResult} * ${userInput.value}`;
+  currentResult *= parseFloat(userInput.value);
+  userInput.value = '';
+  outputResult(currentResult, calculateDescription);
+}
+
+function divide() {
+  const calculateDescription = `${currentResult} / ${userInput.value}`;
+  currentResult /= parseFloat(userInput.value);
+  userInput.value = '';
   outputResult(currentResult, calculateDescription);
 }
 
 addBtn.addEventListener('click', add);
+subtractBtn.addEventListener('click', subtract);
+multiplyBtn.addEventListener('click', multiply);
+divideBtn.addEventListener('click', divide);
