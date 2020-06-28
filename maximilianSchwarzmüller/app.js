e1f1a6a7e8a3722a9f03ca5de603c8b449a8b84c
samhayder <sams.seul@gmail.com>
2020-06-28 22:25:09 +0600

    Two of 29/addBtn function

--- maximilianSchwarzmüller/app.js
+++ maximilianSchwarzmüller/app.js
@@ -2,14 +2,9 @@ const defaultResult = 0;
 let currentResult = defaultResult;
 
 function add(num1, num2) {
-  const result = num1 + num2;
-  alert('This is result:' + result);
+  const calculateDescription = `${currentResult} + ${userInput.value}`;
+  currentResult = currentResult + parseFloat(userInput.value);
+  outputResult(currentResult, calculateDescription);
 }
 
-add(4, 5);
-add(9, 6);
-currentResult = ((currentResult + 10) * 3) / 4 - 1;
-
-let calculationInfo = `(${defaultResult} +10) *3 % 4`;
-
-outputResult(currentResult, calculationInfo);
+addBtn.addEventListener('click', add);
