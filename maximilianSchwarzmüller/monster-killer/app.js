abf7e088df2b5caf9a7af7e15596554a8c33a0c8
samhayder <sams.seul@gmail.com>
2020-07-04 22:13:13 +0600

    Strong Attack Value

--- maximilianSchwarzmüller/monster-killer/app.js
+++ maximilianSchwarzmüller/monster-killer/app.js
@@ -1,5 +1,6 @@
 const ATTACK_VALUE = 10;
 const MONSTER_ATTACK_VALUE = 16;
+const STRONG_ATTACK_VALUE = 19;
 
 let chosenMaxLife = 100;
 let currentMonsterHealth = chosenMaxLife;
@@ -7,8 +8,15 @@ let currentPlayerHealth = chosenMaxLife;
 
 adjustHealthBars(chosenMaxLife);
 
-function attackHandler() {
-  const damages = dealMonsterDamage(ATTACK_VALUE);
+function attackMonster(mode) {
+  let maxDamages;
+  if (mode === 'ATTACK') {
+    maxDamages = ATTACK_VALUE;
+  } else if (mode === 'STRONG_ATTACK') {
+    maxDamages = STRONG_ATTACK_VALUE;
+  }
+  //Attack Handler
+  const damages = dealMonsterDamage(maxDamages);
   currentMonsterHealth -= damages;
   const playerDamages = dealPlayerDamage(MONSTER_ATTACK_VALUE);
   currentPlayerHealth -= playerDamages;
@@ -22,4 +30,13 @@ function attackHandler() {
   }
 }
 
+function attackHandler() {
+  attackMonster('ATTACK');
+}
+
+function strongAttackHandler() {
+  attackMonster('STRONG_ATTACK');
+}
+
 attackBtn.addEventListener('click', attackHandler);
+strongAttackBtn.addEventListener('click', strongAttackHandler);
