f226e1af2e74a862e741f0482e412d53b119029f
samhayder <sams.seul@gmail.com>
2020-09-13 23:19:13 +0600

    Releax App by Traversy Media

---
+++ projects/relexApp-traversy/script.js
@@ -0,0 +1,23 @@
+const container = document.querySelector('.container');
+const text = document.querySelector('.text');
+
+const totalTime = 7500;
+const breatheTime = (totalTime / 5) * 2;
+const holdTime = totalTime / 5;
+
+function breathAnimation() {
+  text.innerText = 'Breathe In!';
+  container.className = 'container grow';
+
+  setTimeout(() => {
+    text.innerText = 'Hold';
+
+    setTimeout(() => {
+      text.innerText = 'Breathe Out!';
+      container.className = 'container shrink';
+    }, holdTime);
+  }, breatheTime);
+}
+
+breathAnimation();
+setInterval(breathAnimation, totalTime);
